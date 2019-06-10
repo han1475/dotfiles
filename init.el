578f8baf55307a319d7802694991c949c76bf558
han1475 <me@han1475.com>
2019-06-10 21:16:56 +0800

    org-page and initial configure

---
+++ init.el
@@ -0,0 +1,63 @@
+;;;init.el --- Load the full configuration -*- coding: utf-8; lexical-binding: t; -*-
+;; Produce backtraces when errors occur
+(setq debug-on-error t)
+
+
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+(add-to-list 'load-path (expand-file-name "lisp/org-page" user-emacs-directory))
+(require 'init-benchmarking) ;; Measure startup time
+
+;;----------------------------------------------------------------------------
+;; Adjust garbage collection thresholds during startup, and thereafter
+;;----------------------------------------------------------------------------
+(let ((normal-gc-cons-threshold (* 20 1024 1024))
+      (init-gc-cons-threshold (* 128 1024 1024)))
+  (setq gc-cons-threshold init-gc-cons-threshold)
+  (add-hook 'emacs-startup-hook
+            (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
+
+;;----------------------------------------------------------------------------
+;; Bootstrap config
+;;----------------------------------------------------------------------------
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(require 'init-utils)
+(require 'init-site-lisp) ;; Must come before elpa, as it may provide package.el
+;; Calls (package-initialize)
+(require 'init-elpa)      ;; Machinery for installing required packages
+(require 'init-exec-path) ;; Set up $PATH
+
+;;----------------------------------------------------------------------------
+;; Load configs for specific features and modes
+;;----------------------------------------------------------------------------
+(require 'init-org-page)
+
+
+;; Extra packages which don't require any configuration
+(require-package 'htmlize)
+(require-package 'dash)
+(require-package 'ht)
+(require-package 'simple-httpd)
+(require-package 'git)
+(require-package 'mustache)
+
+(provide 'init)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (mustache git simple-httpd ht dash htmlize exec-path-from-shell fullframe seq))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+ ;; Local Variables:
+;; coding: utf-8
+;; no-byte-compile: t
+;; End:
+;;; init.el ends here
---
+++ lisp/init-benchmarking.el
@@ -0,0 +1,78 @@
+;;; init-benchmarking.el --- Measure startup and require times -*- coding: utf-8; lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(defun han1475/time-subtract-millis (b a)
+  (* 1000.0 (float-time (time-subtract b a))))
+
+
+(defvar han1475/require-times nil
+  "A list of (FEATURE LOAD-START-TIME LOAD-DURATION).
+LOAD-DURATION is the time taken in milliseconds to load FEATURE.")
+
+(defun han1475/require-times-wrapper (orig feature &rest args)
+  "Note in `han1475/require-times' the time taken to require each feature."
+  (let* ((already-loaded (memq feature features))
+         (require-start-time (and (not already-loaded) (current-time))))
+    (prog1
+        (apply orig feature args)
+      (when (and (not already-loaded) (memq feature features))
+        (let ((time (han1475/time-subtract-millis (current-time) require-start-time)))
+          (add-to-list 'han1475/require-times
+                       (list feature require-start-time time)
+                       t))))))
+
+(advice-add 'require :around 'han1475/require-times-wrapper)
+
+
+(define-derived-mode han1475/require-times-mode tabulated-list-mode "Require-Times"
+  "Show times taken to `require' packages."
+  (setq tabulated-list-format
+        [("Start time (ms)" 20 han1475/require-times-sort-by-start-time-pred)
+         ("Feature" 30 t)
+         ("Time (ms)" 12 han1475/require-times-sort-by-load-time-pred)])
+  (setq tabulated-list-sort-key (cons "Start time (ms)" nil))
+  ;; (setq tabulated-list-padding 2)
+  (setq tabulated-list-entries #'han1475/require-times-tabulated-list-entries)
+  (tabulated-list-init-header)
+  (when (fboundp 'tablist-minor-mode)
+    (tablist-minor-mode)))
+
+(defun han1475/require-times-sort-by-start-time-pred (entry1 entry2)
+  (< (string-to-number (elt (nth 1 entry1) 0))
+     (string-to-number (elt (nth 1 entry2) 0))))
+
+(defun han1475/require-times-sort-by-load-time-pred (entry1 entry2)
+  (> (string-to-number (elt (nth 1 entry1) 2))
+     (string-to-number (elt (nth 1 entry2) 2))))
+
+(defun han1475/require-times-tabulated-list-entries ()
+  (cl-loop for (feature start-time millis) in han1475/require-times
+           with order = 0
+           do (incf order)
+           collect (list order
+                         (vector
+                          (format "%.3f" (han1475/time-subtract-millis start-time before-init-time))
+                          (symbol-name feature)
+                          (format "%.3f" millis)))))
+
+(defun han1475/require-times ()
+  "Show a tabular view of how long various libraries took to load."
+  (interactive)
+  (with-current-buffer (get-buffer-create "*Require Times*")
+    (han1475/require-times-mode)
+    (tabulated-list-revert)
+    (display-buffer (current-buffer))))
+
+
+
+
+(defun han1475/show-init-time ()
+  (message "init completed in %.2fms"
+           (han1475/time-subtract-millis after-init-time before-init-time)))
+
+(add-hook 'after-init-hook 'han1475/show-init-time)
+
+
+(provide 'init-benchmarking)
+;;; init-benchmarking.el ends here
