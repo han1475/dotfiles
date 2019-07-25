e079f400dcea7afc1042e6246ee51e55327f0283
han1475 <me@han1475.com>
2019-07-25 13:51:21 +0800

    change site-domain

--- lisp/init-org-page.el
+++ lisp/init-org-page.el
@@ -5,7 +5,7 @@
 (require 'org-page)
 (setq op/repository-directory "~/site") ;;本地仓库位置
 (setq op/personal-github-link "https://github.com/han1475") ;;github 连接
-(setq op/site-domain "http://han1475.github.io/")
+(setq op/site-domain "http://han1475.com/")
 (setq op/site-main-title "小蜗牛的大梦想")
 (setq op/theme 'dmx) ;; 主题
 
