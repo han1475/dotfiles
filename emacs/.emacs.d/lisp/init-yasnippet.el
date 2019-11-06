8a097e36b271db9048307385dccfc0fc2f719887
han1475 <me@han1475.com>
2019-11-07 01:58:51 +0800

    change code style

--- emacs/.emacs.d/lisp/init-yasnippet.el
+++ emacs/.emacs.d/lisp/init-yasnippet.el
@@ -1,10 +1,8 @@
 ;;; init-yasnippet.el --- Support yasnippet -*- coding: utf-8; lexical-binding: t -*-
 ;;; Commentary:
 ;;; Code:
-
 (require-package 'yasnippet)
 (yas-global-mode 1)
-
 
 
 (provide 'init-yasnippet)
