1cf96fc7b7e3ac49b97b431e1129ccc233d19dd7
han1475 <me@han1475.com>
2019-11-08 15:42:52 +0800

    clean

--- emacs/.emacs.d/snippets/org-mode/.yas-compiled-snippets.el
+++
@@ -1,8 +0,0 @@
-;;; Compiled snippets and support files for `org-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'org-mode
-		     '(("h" "#+HUGO_BASE_DIR: ../\n#+TITLE: $1\n#+DATE: `(format-time-string \"%Y-%m-%d\")`\n#+HUGO_AUTO_SET_LASTMOD: t\n#+HUGO_TAGS: $2\n#+HUGO_CATEGORIES: $3\n#+HUGO_DRAFT: false\n<!--more-->\n\n$0\n" "hugo" nil nil nil "/home/han/.emacs.d/snippets/org-mode/hugo" nil nil)))
-
-
-;;; Do not edit! File generated at Wed Nov  6 23:29:54 2019
