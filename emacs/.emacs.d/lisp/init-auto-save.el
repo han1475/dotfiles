12ebe0f1713872bba3fd3a9a031c05e1da006805
han1475 <me@han1475.com>
2019-12-18 22:40:04 +0800

    fix config of auto-save.

--- emacs/.emacs.d/lisp/init-auto-save.el
+++ emacs/.emacs.d/lisp/init-auto-save.el
@@ -6,7 +6,7 @@
 (auto-save-enable)
 (setq auto-save-silent t)   ; quietly save
 (setq auto-save-delete-trailing-whitespace t)  ; automatically delete spaces at the end of the line when saving
-(auto-save-idle 2) ; auto save every two seconds
+(setq auto-save-idle 2) ; auto save every two seconds
 
 (provide 'init-auto-save)
 ;;; init-auto-save.el ends here
