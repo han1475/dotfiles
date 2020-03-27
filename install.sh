0649f258d86f340e33a9bd9e8922d870ca8bcc2b
han1475 <me@han1475.com>
2020-03-27 20:43:54 +0800

    add cli of backup, restore pkglist and stow.

--- install.sh
+++ install.sh
@@ -1,10 +1,88 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-# Install all the necessary Arch Linux packages, especially `stow`.
-sudo pacman -S \
-     stow 
+set -ue
 
-# Create symbolic links of dotfiles
-stow emacs
-stow oh-my-zsh
-stow git
+###############################################################
+# Local functions
+
+# Install packages that not in pacman repo
+install_packages()
+{
+    # install oh my zsh
+    printf "%s\n" \
+	   "install oh my zsh"
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+}
+restore_packages()
+{
+    sudo pacman -S --needed - < pkglist
+}
+
+backup_packages()
+{
+    sudo pacman -Qqen > pkglist
+}
+
+create_symlink()
+{
+    sudo pacman -S --needed stow
+    #stow git
+    #stow emacs
+    #stow oh-my-zsh
+}
+usage()
+{
+    printf "%s\n" \
+	   "-r, --restore   Restore packages in pkglist file"
+
+    printf "%s\n" \
+	   "-b, --backup    Backup installed by pacman packages to pkglist file"
+
+    printf "%s\n" \
+	   "-s, --symlink   Create symlink of configuration by GNU Stow"
+
+    printf "%s\n" \
+	   "-i, --install   Install packages that in pacman repo"
+
+    printf "%s\n" \
+	   "-h, --help      Display this really usefull message"
+}
+
+###############################################################
+# Command line options
+
+if [ "$#" -eq 0 ]; then
+    printf "%s\\n" "$prog_name: Argument required"
+    printf "%s\\n" "Try '$prog_name --help' for more information."
+    exit 1
+fi
+
+while [ "$#" -gt 0 ] ; do
+    case "$1" in
+	-r | --restore)
+	    restore_packages
+	    shift
+	    ;;
+	-b | --backup)
+	    backup_packages
+	    shift
+	    ;;
+	-s | --symlink)
+	    create_symlink
+	    shift
+	    ;;
+	-i | --install)
+	    install_packages
+	    shift
+	    ;;
+	-h | --help)
+	    usage
+	    shift
+	    ;;
+	*)
+	    usage
+	    printf "\\n%s\\n" "$prog_name: Invalid option '$1'"
+	    exit 1
+	    ;;
+	esac
+done
