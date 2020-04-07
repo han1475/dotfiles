7b27a07c2592795e1d0d3ffce976c9e1f105c2e0
han1475 <me@han1475.com>
2020-04-08 00:30:22 +0800

    add basic init.el

---
+++ init.el
@@ -0,0 +1,15 @@
+;;; init.el --- Load the full configuration -*- lexical-binding: t -*-
+;;; Commentary:
+
+;; This file bootstraps the configuration.
+
+;;; code
+
+
+(provide 'init)
+
+;; Local Variables:
+;; coding: utf-8
+;; no-byte-compile: t
+;; End:
+;;; init.el ends here
