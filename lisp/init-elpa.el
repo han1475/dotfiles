2e4e48acb5c79c4cb53f796ef36a60188803f1bc
han1475 <me@han1475.com>
2020-04-11 17:01:24 +0800

    fix url for melpa-stable

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -8,7 +8,7 @@
 
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
 			 ("melpa" . "https://melpa.org/packages/")
-			 ("melpa-stable" . "https:/stable.melpa.org/packages/")))
+			 ("melpa-stable" . "https://stable.melpa.org/packages/")))
 
 ;; On-demand installation of packages
 (defun require-package (package &optional min-version no-refresh)
