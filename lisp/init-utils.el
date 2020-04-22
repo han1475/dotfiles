241d01ac2c5562da1892b8d0c8e40afcc1bef1c4
han1475 <me@han1475.com>
2020-04-23 00:22:11 +0800

    add after macro for with-eval-after-load

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -36,6 +36,30 @@
 (defun should-use-minimum-resource ()
   (and buffer-file-name
        (string-match-p "\.\\(mock\\|min\\)\.js" buffer-file-name)))
+
+;; from https://github.com/bling/dotemacs/blob/master/core/core-boot.el#L53-L74
+;; see https://github.com/bling/dotemacs#simple-building-block-2
+(defmacro after (feature &rest body)
+  "Executes BODY after FEATURE has been loaded.
+FEATURE may be any one of:
+    'evil            => (with-eval-after-load 'evil BODY)
+    \"evil-autoloads\" => (with-eval-after-load \"evil-autolaods\" BODY)
+    [evil cider]     => (with-eval-after-load 'evil
+                          (with-eval-after-load 'cider
+                            BODY))
+"
+  (declare (indent 1))
+  (cond
+   ((vectorp feature)
+    (let ((prog (macroexp-progn body)))
+      (cl-loop for f across feature
+               do
+               (progn
+                 (setq prog (append `(',f) `(,prog)))
+                 (setq prog (append '(with-eval-after-load) prog))))
+      prog))
+   (t
+    `(with-eval-after-load ,feature ,@body))))
        
 
 (provide 'init-utils)
