720219793464bad148f93c8c8d9c0df9f14e32eb
han1475 <me@han1475.com>
2020-04-25 18:31:53 +0800

    add config for measure startup time and garbage collections

--- init.el
+++ init.el
@@ -5,6 +5,15 @@
 
 ;;; code
 
+;; Measure startup time and garbage collections
+(add-hook 'emacs-startup-hook
+	  (lambda ()
+	    (message "Emacs ready in %s with %d garbage collections."
+		     (format "%.2f seconds"
+			     (float-time
+			      (time-subtract after-init-time before-init-time)))
+		     gcs-done)))
+
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
 ;;----------------------------------------------------------------------------
