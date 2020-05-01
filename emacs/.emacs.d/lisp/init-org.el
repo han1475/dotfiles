1626c616d0addf5d28f73f681da19d96d55b5b18
han1475 <me@han1475.com>
2020-05-01 21:41:22 +0800

    clean emacs directory for use git submodule

--- emacs/.emacs.d/lisp/init-org.el
+++
@@ -1,6 +0,0 @@
-;;; init-org.el --- Suport for org -*- lexical-binding: t -*-
-;;; Commentary:
-;;; Code
-(setq org-log-done 'time)
-(provide 'init-org)
-;;; init-org.el ends here
--- emacs/.emacs.d/lisp/init-yasnippet.el
+++
@@ -1,9 +0,0 @@
-;;; init-yasnippet.el --- Support yasnippet -*- coding: utf-8; lexical-binding: t -*-
-;;; Commentary:
-;;; Code:
-(require-package 'yasnippet)
-(setq my-yasnippets (expand-file-name "~/.emacs.d/snippet"))
-(yas-global-mode 1)
-
-(provide 'init-yasnippet)
-;;; init-yasnippet.el ends here
