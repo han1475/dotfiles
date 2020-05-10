255840f00b91b58009b69715c7d98bf8ebc3d386
han1475 <me@han1475.com>
2020-05-10 23:01:49 +0800

    add basic bingdings of hydra

---
+++ bindings/modes/hydras.el
@@ -0,0 +1,11 @@
+;;; hydras.el ---  hydra bindings configuration -*- lexical-binding: t -*-
+;;; Commentary:
+;;; code:
+
+(provide 'hydras)
+
+;; Local Variables:
+;; coding: utf-8
+;; no-byte-compile: t
+;; End:
+;;; hydras.el ends here
