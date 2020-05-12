f544fb46875b94774a1dd03ca372d64c3f45734a
han1475 <me@han1475.com>
2020-05-12 16:15:50 +0800

    add bindings for errors

--- bindings/modes/hydras.el
+++ bindings/modes/hydras.el
@@ -2,6 +2,31 @@
 ;;; Commentary:
 ;;; code:
 
+;;; flycheck used by default for errors
+(defvar /hydras/errors/flycheck nil)
+(defun /hydras/errors/target-list ()
+  "Return target list."
+  (if /hydras/errors/flycheck
+      'flycheck
+    'emacs))
+(defhydra /hydras/errors (:hint nil)
+    "
+   errors:  navigation                 flycheck
+            -----------------------    ---------------
+            _j_ → next error             _l_ → list errors
+            _k_ → previous error         _?_ → describe checker
+            _t_ → toggle list (%(/hydras/errors/target-list))
+"
+    ("j" (if /hydras/errors/flycheck
+	     (call-interactively #'flycheck-next-error)
+	   (call-interactively #'next-error)))
+    ("k" (if /hydras/errors/flycheck
+	     (call-interactively #'flycheck-previous-error)
+	   (call-interactively #'previous-error)))
+    ("t" (setq /hydras/errors/flycheck (not /hydras/errors/flycheck)))
+    ("?" flycheck-describe-checker)
+      ("l" flycheck-list-errors :exit t))
+
 (provide 'hydras)
 
 ;; Local Variables:
