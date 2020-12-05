f8d0b8ea37f16ac54c8c86218a8d6ef2e4843ea4
han1475 <me@han1475.com>
2020-12-05 22:37:58 +0800

    refactor(emacs): move config of yasnippet to config.org

--- emacs/dot-emacs.d/config/config-utils.el
+++ emacs/dot-emacs.d/config/config-utils.el
@@ -2,37 +2,6 @@
 ;;; Commentary:
 ;;; code:
 
-(defvar cached-normal-file-full-path nil)
-(defvar load-user-customized-major-mode-hook t)
-(defvar force-buffer-file-temp-p nil)
-
-(defun is-buffer-file-temp ()
-  "If (buffer-file-name) is nil or a temp file or HTML file converted from org file."
-  (interactive)
-  (let* ((f (buffer-file-name)) (rlt t))
-    (cond
-     ((not load-user-customized-major-mode-hook)
-      (setq rlt t))
-     ((not f)
-      ;; file does not exist at all
-      ;; org-babel edit inline code block need calling hook
-      (setq rlt nil))
-     ((string= f cached-normal-file-full-path)
-      (setq rlt nil))
-     ((string-match (concat "^" temporary-file-directory) f)
-      ;; file is create from temp directory
-      (setq rlt t))
-     ((and (string-match "\.html$" f)
-           (file-exists-p (replace-regexp-in-string "\.html$" ".org" f)))
-      ;; file is a html file exported from org-mode
-      (setq rlt t))
-     (force-buffer-file-temp-p
-      (setq rlt t))
-     (t
-      (setq cached-normal-file-full-path f)
-      (setq rlt nil)))
-    rlt))
-
 (defun should-use-minimum-resource ()
   (and buffer-file-name
        (string-match-p "\.\\(mock\\|min\\)\.js" buffer-file-name)))
