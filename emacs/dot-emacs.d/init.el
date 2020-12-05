f250ec4d56868a47b3e1514b2925582dafa2b649
han1475 <me@han1475.com>
2020-12-05 17:22:50 +0800

    style(emacs): format code with formatting of emacs

--- emacs/dot-emacs.d/init.el
+++ emacs/dot-emacs.d/init.el
@@ -6,21 +6,21 @@
 ;;; code
 
 (let  ((normal-gc-cons-threshold (* 20 1024 1024))
-      (init-gc-cons-threshold (* 128 1024 1024)))
-
-      ;;Adjust garbage collection thresholds during startup, and thereafter
-      (setq gc-cons-threshold init-gc-cons-threshold)
-      (add-hook 'emacs-startup-hook
-          (lambda () (setq gc-cons-threshold normal-gc-cons-threshold)))
-
-      ;; Measure startup time and garbage collections
-      (add-hook 'emacs-startup-hook
-      	  (lambda ()
-	         (message "Emacs ready in %s with %d garbage collections."
-       		     (format "%.2f seconds"
-	    		     (float-time
-	    		      (time-subtract after-init-time before-init-time)))
-	    	     gcs-done))))
+       (init-gc-cons-threshold (* 128 1024 1024)))
+  
+  ;;Adjust garbage collection thresholds during startup, and thereafter
+  (setq gc-cons-threshold init-gc-cons-threshold)
+  (add-hook 'emacs-startup-hook
+            (lambda () (setq gc-cons-threshold normal-gc-cons-threshold)))
+  
+  ;; Measure startup time and garbage collections
+  (add-hook 'emacs-startup-hook
+      	    (lambda ()
+	      (message "Emacs ready in %s with %d garbage collections."
+       		       (format "%.2f seconds"
+	    		       (float-time
+	    			(time-subtract after-init-time before-init-time)))
+	    	       gcs-done))))
 
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
 
