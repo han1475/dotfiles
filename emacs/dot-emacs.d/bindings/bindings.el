4f3245433824222d72cd3520c22aa8dfd815cd4c
han1475 <me@han1475.com>
2020-12-06 13:43:21 +0800

    refactor(emacs): move config of which-key to config.org

--- emacs/dot-emacs.d/bindings/bindings.el
+++ emacs/dot-emacs.d/bindings/bindings.el
@@ -2,12 +2,6 @@
 ;;; Commentary:
 ;;; code:
 
-;; Displays the key bindings following your currently entered incomplete command
-(require-package 'which-key)
-(setq which-key-idle-delay 0.2)
-(setq which-key-min-display-lines 3)
-(which-key-mode)
-
 ;; Use a fancy separator between Dedicated window for
 ;; Hydra hints and the Echo Area
 (setq lv-use-separator t)
